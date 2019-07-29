80e5103afad3d40e896738b55eb21483e00c46eb
meyassu <51754935+meyassu@users.noreply.github.com>
2019-07-29 14:54:22 -0400

    nested objects works but bad order

--- serialization/protobuf/ProtoConverter.java
+++ serialization/protobuf/ProtoConverter.java
@@ -2,11 +2,9 @@ import java.io.*;
 import java.util.*;
 import java.lang.reflect.*;
 /*
-convert object to protobuffer format and write to txt file. Then stick txt file into bytebuffer somehow, then transfer over to hikey\
+convert object to protobuffer format and write to txt file. Then stick txt file (cbor?) into bytebuffer somehow, then transfer over to hikey\
 On hikey end, re-inflate txt to object and manipulate
-
-Protobuf may not be best solutions for few reasons
-   no existing library to automatically convert obj -> protobuf.txt 
+ 
 */
 public class ProtoConverter{
 
@@ -46,32 +44,42 @@ public class ProtoConverter{
       return f;
    }
    private static String generateAttributes(Map<String,String> namingConventions, Object o,String s)throws Exception{
-      int i = 0;
-      List<Method> methods = Arrays.asList(o.getClass().getMethods());
-      for(Method m : methods){
-         if(m.getName().substring(0,3).equals("get") && m.getParameterTypes().length == 0){
-            if(m.getName().equals("getClass"))
-               continue;
-            Object r = m.invoke(o);
-            String dataType = (r.getClass() + "").substring((r.getClass() + "").lastIndexOf(".") + 1);
-            String protoKw = namingConventions.get(dataType);
-            String dataName = " " + m.getName().substring(3,4).toLowerCase() + m.getName().substring(4) + " = " + i++ + ";\n";
-            boolean builtIn = (protoKw == null) ? false:true;
-            if(builtIn)//built-in
-               s += protoKw + dataName;
-            else{
-               if(namingConventions.get(methods.get(methods.size()-1).invoke(o).) != null){
-                  methods.remove(m);
-                  methods.add(m);//add to end
-               }
+         int i = 0;
+         List<Method> methods = Arrays.asList(o.getClass().getMethods());
+         for(Method m : methods){
+            if(m.getName().substring(0,3).equals("get") && m.getParameterTypes().length == 0){
+               if(m.getName().equals("getClass"))
+                  continue;
+               String varName = " " + m.getName().substring(3,4).toLowerCase() + m.getName().substring(4) + " = " + i++ + ";\n";
+               Object r = m.invoke(o);
+               String protoKw = getProtoKeyword(namingConventions,r);
+               boolean builtIn = (protoKw == null) ? false:true;
+               if(builtIn)
+                  s += protoKw + varName;
                else{
-                  s += "message " + (r.getClass() + "") + "{"; 
-                  s += generateAttributes(namingConventions,r,"") + "}\n"; 
+               //check whether obj from last method is builtIn;if yes swap,else process
+               //below way better approach to get return type; no string manipulation
+                  Method last = methods.get(methods.size()-1);
+                  String lastRt = last.getReturnType().toString();
+                  if(namingConventions.get(lastRt) != null){
+                     methods.remove(m);
+                     methods.add(m);
+                  }
+                  else{
+                     s += "message " + (r.getClass() + "") + "{"; 
+                     s += generateAttributes(namingConventions,r,"") + "}\n"; 
+                  }
                }
             }
          }
-      }
-      return s;
+         return s;
    }
    
+   private static String getProtoKeyword(Map<String,String> namingConventions,Object r){
+      String classo = r.getClass() + "";
+      String dt = classo.substring(classo.lastIndexOf(".") + 1);
+      String nm = namingConventions.get(dt);
+      return nm;
+      //return namingConventions.get(r.getClass() + "").substring((r.getClass() + "").lastIndexOf(".") + 1);
+   }
 }
