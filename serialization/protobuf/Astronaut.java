a0d6023ecbe365603f6392213c6a08227c62f5e3
meyassu <51754935+meyassu@users.noreply.github.com>
2019-07-29 11:54:03 -0400

    serialization

---
+++ serialization/protobuf/Astronaut.java
@@ -0,0 +1,17 @@
+public class Astronaut{
+
+  String name;
+  int height;
+  int weight;
+  
+  public Astronaut(){
+     name = "Buzz Lightyear";
+     height = 0;
+     weight = 0;
+  } 
+  
+  public String getName(){return this.name;}
+  public int getHeight(){return this.height;}
+  public int getWeight(){return this.weight;}
+  
+  }
---
+++ serialization/protobuf/ProtoConverter.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+/*
+convert object to protobuffer format and write to txt file. Then stick txt file into bytebuffer somehow, then transfer over to hikey\
+On hikey end, re-inflate txt to object and manipulate
+
+Protobuf may not be best solutions for few reasons
+   no existing library to automatically convert obj -> protobuf.txt 
+*/
+public class ProtoConverter{
+
+   public static void main(String [] args)throws Exception{
+      Astronaut a = new Astronaut();
+      Rocket r = new Rocket(a,"FalconHeavy",5,10000,3);
+      
+      Map<String,String> h = new HashMap<>();
+      h.put("Integer","int32");
+      h.put("String","String");
+      File f = convert(h,"rocket.txt",r,2,"NASA",false,"",false,"");
+      Scanner scan = new Scanner(f);
+      String terminalOutput = "";
+      f.delete();
+   }
+   
+   public static File convert(Map<String,String> namingConventions,String filename,Object o,int version,String pkgName,boolean java_package,String java_pkg,boolean outer_class_name,String outerName) throws Exception{
+      File f = new File(filename);
+      if(f.createNewFile())
+         System.out.println("File created");
+      else{
+         f = new File(filename + Math.random()*10);
+      }   
+      FileWriter writer = new FileWriter(f);
+      String s = "syntax = \"proto" + version + "\";\n\npackage " + pkgName + ";\n";
+      if(java_package)
+         s += "\n\noption java_package = \"" + java_pkg + "\";\n";
+      if(outer_class_name)
+         s+= "\noption java_outer_classname = \"" + outerName + "\";\n";
+      
+      s += "\nmessage " + (o.getClass() + "").substring(6) + " {\n";
+      String output = generateAttributes(namingConventions,o,"");
+      s += output + "\n}";
+      System.out.println(s);
+      writer.write(s);
+      writer.close();
+      return f;
+   }
+   private static String generateAttributes(Map<String,String> namingConventions, Object o,String s)throws Exception{
+      int i = 0;
+      List<Method> methods = Arrays.asList(o.getClass().getMethods());
+      for(Method m : methods){
+         if(m.getName().substring(0,3).equals("get") && m.getParameterTypes().length == 0){
+            if(m.getName().equals("getClass"))
+               continue;
+            Object r = m.invoke(o);
+            String dataType = (r.getClass() + "").substring((r.getClass() + "").lastIndexOf(".") + 1);
+            String protoKw = namingConventions.get(dataType);
+            String dataName = " " + m.getName().substring(3,4).toLowerCase() + m.getName().substring(4) + " = " + i++ + ";\n";
+            boolean builtIn = (protoKw == null) ? false:true;
+            if(builtIn)//built-in
+               s += protoKw + dataName;
+            else{
+               if(namingConventions.get(methods.get(methods.size()-1).invoke(o).) != null){
+                  methods.remove(m);
+                  methods.add(m);//add to end
+               }
+               else{
+                  s += "message " + (r.getClass() + "") + "{"; 
+                  s += generateAttributes(namingConventions,r,"") + "}\n"; 
+               }
+            }
+         }
+      }
+      return s;
+   }
+   
+}
