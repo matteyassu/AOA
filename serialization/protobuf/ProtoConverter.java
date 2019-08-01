7ae944e6064e2406836ce19b7cf203dbd68b47a4
meyassu <51754935+meyassu@users.noreply.github.com>
2019-08-01 12:05:27 -0400

    Rocket.java converted to proto-formatted String; next steps: funnel output to .txt,convert to .proto,and programmitcally put through compiler

--- serialization/protobuf/ProtoConverter.java
+++ serialization/protobuf/ProtoConverter.java
@@ -3,8 +3,13 @@ import java.util.*;
 import java.lang.reflect.*;
 /*
 convert object to protobuffer format and write to txt file. Then stick txt file (cbor?) into bytebuffer somehow, then transfer over to CAT
-On CAT end, re-inflate txt to object and manipulate/display
- 
+On CAT end, re-inflate txt to object and manipulate/display 
+*/
+
+/*
+TODO: indentation
+      change to .proto file
+      automate protoc
 */
 public class ProtoConverter{
 
@@ -13,10 +18,13 @@ public class ProtoConverter{
       Rocket r = new Rocket(a,"FalconHeavy",5,10000,3);
       
       Map<String,String> h = new HashMap<>();
-      h.put("Integer","int32");
-      h.put("String","String");
       h.put("int","int32");
-      
+      h.put("Integer","int32");
+      h.put("class java.lang.Integer","int32");
+      h.put("String","String");
+      h.put("String","String");
+      h.put("class java.lang.String","String");
+      h.put("boolean","bool");
       File f = convert(h,"rocket.txt",r,2,"NASA",false,"",false,"");
       Scanner scan = new Scanner(f);
       String terminalOutput = "";
@@ -31,57 +39,117 @@ public class ProtoConverter{
          f = new File(filename + Math.random()*10);
       }   
       FileWriter writer = new FileWriter(f);
-      String s = "syntax = \"proto" + version + "\";\n\npackage " + pkgName + ";\n";
+      String s = "syntax = \"proto" + version + "\";\r\n\r\npackage " + pkgName + ";\r\n";
       if(java_package)
-         s += "\n\noption java_package = \"" + java_pkg + "\";\n";
+         s += "\r\n\r\noption java_package = \"" + java_pkg + "\";\r\n";
       if(outer_class_name)
-         s+= "\noption java_outer_classname = \"" + outerName + "\";\n";
-      
-      s += "\nmessage " + (o.getClass() + "").substring(6) + " {\n";
-      String output = generateAttributes(namingConventions,o,"");
-      s += output + "\n}";
+         s+= "\r\noption java_outer_classname = \"" + outerName + "\";\r\n";
+      String output = generateAttributes(namingConventions,o,0);
+      s += output + "\r\n}";
       System.out.println(s);
       writer.write(s);
       writer.close();
       return f;
    }
-   private static String generateAttributes(Map<String,String> namingConventions, Object o,String s)throws Exception{
-      int i = 0;
-      List<Method> methods = Arrays.asList(o.getClass().getMethods());
-      for(Method m : methods){
-         String methodName = m.getName(); 
-         if(methodName.substring(0,3).equals("get") && m.getParameterTypes().length == 0){
-            if(m.getName().equals("getClass"))
+   private static String generateAttributes(Map<String,String> namingConventions, Object o,int numIndent)throws Exception{
+      String attributes = addIndent(numIndent) + "message " + (o.getClass() + "").substring(6) + " {\r\n";
+      numIndent++;
+      Field [] fields = o.getClass().getDeclaredFields();
+      Object p = null; 
+      for(int i = 0; i < fields.length; i++) {
+         String fieldType = fields[i].getType().toString();
+         String fieldName = fields[i].getName();
+         if(namingConventions.get(fieldType) == null) {
+         	//swap to ensure complex objects are enumerated last
+            boolean swapped = false;
+            for(int j = fields.length - 1; j > i; j--) {
+               if(namingConventions.get(fields[j].getType().toString()) != null) {
+                  Field temp = fields[j];
+                  fields[j] = fields[i];
+                  fields[i] = temp;
+                  swapped = true;
+                  break;
+               }
+            }
+            if(swapped) {
+               fieldType = fields[i].getType().toString();
+               fieldName = fields[i].getName();
+               attributes += addIndent(numIndent) + namingConventions.get(fieldType) + " " + fieldName + " = " + i + ";\r\n";
                continue;
-               //gather components of method signature
-               //ex:engineType
-            String returnVar = methodName.substring(3,4).toLowerCase() + methodName.substring(4);
-               //ex:String
-            String returnType = m.getReturnType().toString();
-            boolean builtIn = false;
-            if(namingConventions.get(returnType) != null)
-               builtIn = true;
-            else if(returnType.contains(".")){
-               //if comes in class java.lang.String format
-               returnType = returnType.substring(returnType.lastIndexOf(".")+1);
-               if(namingConventions.get(returnType) != null)
-                  builtIn = true;
             }
-               //primitive
-            if(builtIn)
-               s += processPrimitive(namingConventions,returnType,returnVar,i);
-               //complex
-            else{
-               int endIndex = methods.indexOf(m);
-               s += processComplex(namingConventions,methods,o,"",endIndex); 
+            try {
+               p = fields[i].get(o);
             }
+            catch(IllegalAccessException e) {
+               e.printStackTrace();
+            }
+            attributes += "\r\n" + generateAttributes(namingConventions,p,numIndent);
          }
+         else
+            attributes += addIndent(numIndent) + namingConventions.get(fieldType) + " " + fieldName + " = " + i + ";\r\n";
       }
-      return s;
+   	// try {
+   // 			writer.write(attributes);
+   // 		}
+   // 		catch(IOException e) {
+   // 			e.printStackTrace();
+   // 		}
+      return attributes;
+   } 
+   private static String addIndent(int numIndent) {
+      String indent = "";
+      for(int i = 0; i < numIndent; i++) 
+         indent += "  ";
+      return indent;
    }
+     //  Field [] fields = o.getClass().getDeclaredFields();
+//       String attributes = "";
+//       Object p = null;
+//       for(int i = 0; i < fields.length; i++) {
+// 			String fieldType = fields[i].getType().toString();
+// 			String fieldName = fields[i].getName();
+// 			if(namingConventions.get(fieldType) == null) {
+// 				p = fields[i].get(o);
+// 				//attributes += generateAttributes(p,attributes);
+// 			}
+// 		}
+//       
+//       int i = 0;
+//       List<Method> methods = Arrays.asList(o.getClass().getMethods());
+//       for(Method m : methods){
+//          String methodName = m.getName(); 
+//          if(methodName.substring(0,3).equals("get") && m.getParameterTypes().length == 0){
+//             if(m.getName().equals("getClass"))
+//                continue;
+//                //gather components of method signature
+//                //ex:engineType
+//             String returnVar = methodName.substring(3,4).toLowerCase() + methodName.substring(4);
+//                //ex:String
+//             String returnType = m.getReturnType().toString();
+//             boolean builtIn = false;
+//             if(namingConventions.get(returnType) != null)
+//                builtIn = true;
+//             else if(returnType.contains(".")){
+//                //if comes in class java.lang.String format
+//                returnType = returnType.substring(returnType.lastIndexOf(".")+1);
+//                if(namingConventions.get(returnType) != null)
+//                   builtIn = true;
+//             }
+//                //primitive
+//             if(builtIn)
+//                s += processPrimitive(namingConventions,returnType,returnVar,i);
+//                //complex
+//             else{
+//                int endIndex = methods.indexOf(m);
+//                s += processComplex(namingConventions,methods,o,"",endIndex); 
+//             }
+//          }
+//       }
+//       return s;
+   //}
    
    private static String processPrimitive(Map<String,String> namingConventions,String returnType,String returnVar,int i){
-      return namingConventions.get(returnType) + " " + returnVar + " = " + i + ";\n";
+      return namingConventions.get(returnType) + " " + returnVar + " = " + i + ";\r\n";
    }
    
    // private static boolean builtIn(Map<String,String> namingConventions,String returnType){
@@ -89,6 +157,9 @@ public class ProtoConverter{
 //    }
    private static String processComplex(Map<String,String> namingConventions,List<Method> methods,Object o,String s,int endIndex)throws Exception{
       //execute swapping algo and call processPrimitive() OR swapping not possible and execute processComplex()
+      
+      
+      
       for(int i = methods.size()-1; i >= endIndex; i--){
          //if get(),0 param,and built in swap
          String name = methods.get(i).getName();
@@ -113,35 +184,35 @@ public class ProtoConverter{
       } 
        
       String className = methods.get(endIndex).getReturnType().toString().substring(6); 
-      s += "message " + className + "{ \n";
+      s += "\r\nmessage " + className + "{ \r\n";
       List<Method> complexMethods = Arrays.asList(methods.get(endIndex).getReturnType().getMethods());
       for(Method m : complexMethods){
          String name = m.getName();
          if(m.getName().substring(0,3).equals("get") && m.getParameterTypes().length == 0){
             if(m.getName().equals("getClass"))
                continue;
-         String returnType = m.getReturnType().toString();
-         boolean builtIn = false;
-         if(namingConventions.get(returnType) != null)
-            builtIn = true;
-         else if(returnType.contains(".")){
-               //if comes in class java.lang.String format
-            returnType = returnType.substring(returnType.lastIndexOf(".")+1);
+            String returnType = m.getReturnType().toString();
+            boolean builtIn = false;
             if(namingConventions.get(returnType) != null)
                builtIn = true;
-         }
-         if(builtIn){
-            String returnVar = m.getName();
-            s += processPrimitive(namingConventions,returnType,m.getName().substring(3,4).toLowerCase() + m.getName().substring(4),1);
-         }      
-         else{
-            Object r = complexMethods.get(endIndex).getReturnType();
-            s += "\nmessage " + (r.getClass() + "").substring(6) + " {\n"; 
-            processComplex(namingConventions,complexMethods,r,s,complexMethods.indexOf(m));
+            else if(returnType.contains(".")){
+               //if comes in class java.lang.String format
+               returnType = returnType.substring(returnType.lastIndexOf(".")+1);
+               if(namingConventions.get(returnType) != null)
+                  builtIn = true;
+            }
+            if(builtIn){
+               String returnVar = m.getName();
+               s += processPrimitive(namingConventions,returnType,m.getName().substring(3,4).toLowerCase() + m.getName().substring(4),1);
+            }      
+            else{
+               Object r = complexMethods.get(endIndex).getReturnType();
+               s += "\r\n\r\nmessage " + (r.getClass() + "").substring(6) + " {\r\n"; 
+               processComplex(namingConventions,complexMethods,r,s,complexMethods.indexOf(m));
+            }
          }
       }
-      }
-      return s;
+      return s + "}\r\n";
    }
    
    private static void swap(List<Method> methods,int i, int j){
---
+++ serialization/protobuf/ProtoConverterOOP.java
@@ -0,0 +1,171 @@
+/*
+ * Notes: Check for String.equals("") instead of using booleans in skeleton generation?
+ * 		  Added features:
+ * 			required vs optional variables
+ * 			repeated variables
+ * 			indentation
+ * 			
+ */
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+public class ProtoConverterOOP {
+   
+   public static void main(String [] args){
+      Astronaut a = new Astronaut();
+      Rocket r = new Rocket(a,"FalconHeavy",5,10000,3);
+      ProtoConverterOOP proto = new ProtoConverterOOP(r);
+      String skeleton = proto.generateSkeleton(2,"NASA",true,"com.example.proto",false,"");
+      String attributes = proto.generateAttributes(r,0);
+      System.out.println(skeleton + attributes);
+      FileWriter writer = proto.getWriter();
+      try {
+         writer.write(skeleton);
+         writer.write(attributes);
+      }
+      catch(IOException i) {
+         i.printStackTrace();
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }
+   }
+   
+   private Map<String,String> typeConventions;
+   private Object o;
+   private File protoFile;
+   private FileWriter writer;
+   
+   //constructor: map,object,file,writer initialization 
+   public ProtoConverterOOP(Object o) {
+      typeConventions = new HashMap<String,String>();
+      typeConventions.put("Integer","int32");
+      typeConventions.put("class java.lang.Integer","int32");
+      typeConventions.put("String","String");
+      typeConventions.put("String","String");
+      typeConventions.put("class java.lang.String","String");
+      typeConventions.put("boolean","bool");
+      typeConventions.put("int","int32");
+   	
+      this.o = o;
+   	
+      generateFile();
+   	
+      generateFileWriter();
+   }
+   //getters
+   public Map<String,String> getTypeConventions(){
+      return typeConventions;
+   }
+   public Object getObject(){
+      return o;
+   }
+   public FileWriter getWriter(){
+      return writer;
+   }
+   private boolean generateFile() {
+      Scanner scan = new Scanner(System.in);
+      boolean createFile = false;
+   	//multiple chances for filename input
+      while(!createFile) {
+         System.out.print("Please enter a filename: ");
+         String filename = scan.next(); 
+         try {
+            protoFile = new File(filename);
+            createFile = protoFile.createNewFile();
+            createFile = true;
+         }
+         catch(IOException i) {
+            i.printStackTrace();
+         }
+      }
+      scan.close();
+      return createFile;
+   }
+   private boolean generateFileWriter() {
+      try {
+         writer = new FileWriter(protoFile);
+         return true;
+      }
+      catch(IOException i) {
+         i.printStackTrace();
+         return false;
+      }
+   }
+   public String generateSkeleton(int version,String pkgName,boolean javaPkg,String javaPkgName,boolean outerClass,String outerClassName) {
+      String doubleSpace = "\r\n\r\n";
+      String skeleton = "syntax = \"proto" + version + ";" + doubleSpace;
+   	
+   	//packages
+      if(!javaPkg)
+         javaPkgName = pkgName;
+      skeleton += "package " + pkgName + ";" + doubleSpace;
+      skeleton += "option java_package = \"" + javaPkgName + "\";\r\n";
+   	
+   	//outer_class_name
+      if(!outerClass)
+         outerClassName = this.getClass().getSimpleName();
+      skeleton += "option java_outer_classname = \"" + outerClassName + "\";" + doubleSpace;
+      
+      return skeleton;
+   }
+	
+	//recursive solution (n^n complexity if all fields are complex types; not a result of algorithmic design but inherent to problem i.e unavoidable)
+   public String generateAttributes(Object o,int numIndent) {
+      String attributes = "message " + (o.getClass() + "").substring(6) + " {\r\n";
+      numIndent++;
+      Field [] fields = o.getClass().getDeclaredFields();
+      Object p = null; 
+      for(int i = 0; i < fields.length; i++) {
+         String fieldType = fields[i].getType().toString();
+         String fieldName = fields[i].getName();
+         if(typeConventions.get(fieldType) == null) {
+         	//swap to ensure complex objects are enumerated last
+            boolean swapped = false;
+            for(int j = fields.length - 1; j > i; j--) {
+               if(typeConventions.get(fields[j].getType().toString()) != null) {
+                  Field temp = fields[j];
+                  fields[j] = fields[i];
+                  fields[i] = temp;
+                  swapped = true;
+                  break;
+               }
+            }
+            if(swapped) {
+               fieldType = fields[i].getType().toString();
+               fieldName = fields[i].getName();
+               attributes += addIndent(numIndent) + typeConventions.get(fieldType) + " " + fieldName + " = " + i + ";\r\n";
+               continue;
+            }
+            try {
+               p = fields[i].get(o);
+            }
+            catch(IllegalAccessException e) {
+               e.printStackTrace();
+            }
+            //complex object
+            attributes += addIndent(numIndent) + generateAttributes(p,numIndent) + addIndent(numIndent);
+            attributes += addIndent(--numIndent) + "\n}";
+         }
+         //simple object
+         else{
+            attributes += addIndent(numIndent) + typeConventions.get(fieldType) + " " + fieldName + " = " + i + ";\r\n";
+            if( i == fields.length -1)
+               attributes += addIndent(--numIndent) + "}";
+            } 
+      }
+      return attributes;
+   }
+   private String addIndent(int numIndent) {
+      String indent = "";
+      for(int i = 0; i < numIndent; i++)
+         indent += "  ";
+      return indent;
+   }
+   private boolean txtProto() {
+   	
+      return true;
+   }
+	
+		
+}
